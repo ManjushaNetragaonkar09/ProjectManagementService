0b7d4e6899672427dcdbf8b448be923e6b996a44
ManjushaNetragaonkar <manjushanetragaonkar128@gmail.com>
2023-03-08 23:21:00 +0530

    structure added

---
+++ src/main/java/com/datapoem/demo/ProjectManagementServiceMainClass.java
@@ -0,0 +1,13 @@
+package com.datapoem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementServiceMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementServiceMainClass.class, args);
+	}
+
+}
