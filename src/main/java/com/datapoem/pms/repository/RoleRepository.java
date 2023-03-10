94b46738f09d4b426058d3f4c5fe4036bf17d4f8
ManjushaNetragaonkar <manjushanetragaonkar128@gmail.com>
2023-03-10 21:45:05 +0530

    jwt with DaoAuthenticationProvider added

---
+++ src/main/java/com/datapoem/pms/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.datapoem.pms.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.datapoem.pms.models.ERole;
+import com.datapoem.pms.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
